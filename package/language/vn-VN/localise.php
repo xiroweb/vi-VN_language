e6fb34af2c2c859e62c035e7cfc88bfe15db9d58
XiroWeb <xiroweb@gmail.com>
2021-12-01 19:46:44 +0700

    add function fix language vietnam

--- package/language/vn-VN/localise.php
+++ package/language/vn-VN/localise.php
@@ -6,6 +6,8 @@
  * @license    GNU General Public License version 2 or later; see LICENSE.txt
  */
 
+use Joomla\String\StringHelper;
+
 defined('_JEXEC') or die;
 
 /**
@@ -38,5 +40,30 @@ abstract class Vn_VNLocalise
 		{
 			return array('OTHER', 'MORE');
 		}
+	}
+
+	public static function transliterate($string)
+	{
+		$str = StringHelper::strtolower($string);
+		//Specific language transliteration.
+		//This one is for latin 1, latin supplement , extended A, Cyrillic, Greek
+
+		$glyph_array = array(
+			'a' => 'á,à,ả,ã,ạ,À,Á,Ả,Ã,Ạ,â,ầ,ấ,ẩ,ẫ,ậ,Â,Ầ,Ấ,Ẩ,Ẫ,Ậ,ă,ằ,ắ,ẳ,ẳ,ặ',
+			'd' => 'Ð,đ',
+			'e' => 'è,é,ẻ,ẽ,ẹ,È,É,Ẻ,Ẽ,Ẹ,ê,ề,ế,ể,ễ,ệ,Ê,Ề,Ế,Ể,Ễ,Ệ',
+			'i' => 'í,ì,ỉ,ĩ,ị,Ì,Í,Ỉ,Ĩ,Ị',
+			'o' => 'ò,ó,ỏ,õ,ọ,ơ,ờ,ớ,ở,ỡ,ợ,Ò,Ó,Ỏ,Õ,Ọ,Ơ,Ờ,Ớ,Ở,Ỡ,Ợ,ô,ồ,ố,ổ,ỗ,ộ,Ồ,Ố,Ổ,Ỗ,Ổ,Ộ',
+			'u' => 'ù,ú,ủ,ũ,ụ,Ù,Ú,Ủ,Ũ,ư,ừ,ứ,ử,ữ,ự,Ư,Ừ,Ứ,Ử,Ữ,Ự',
+			'y' => 'ý,ỷ,ỳ,ỹ,ỵ,Ỹ,Ỷ,Ỵ,Ý,Ỳ'
+		);
+
+		foreach ($glyph_array as $letter => $glyphs)
+		{
+			$glyphs = explode(',', $glyphs);
+			$str = str_replace($glyphs, $letter, $str);
+		}
+
+		return $str;
 	}
 }
